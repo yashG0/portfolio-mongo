4329a0634172ea6e29e2f805f8ef33a748a0c22a
Your Name <yashgaurkar0@gmail.com>
2023-09-29 22:48:51 +0530

    env closed

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
 import mongoose from 'mongoose'
 
 
-export const mongoUrl = `mongodb+srv://yashG0:VYX01mzOuCRcX8kc@cluster0.zrbe5qg.mongodb.net/form_data_db?retryWrites=true&w=majority`;
+export const mongoUrl = process.env.MONGODB_URL;
