11b2b536a2d52d888ee833924a156bc17127263f
Your Name <yashgaurkar0@gmail.com>
2023-09-30 10:20:00 +0530

    new route changes

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -3,16 +3,27 @@ import { Product } from "@/lib/models/product";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    await mongoose.connect(mongoUrl);
-    const data = await Product.find({ name: "yash" });
-    return NextResponse.json({ result: data });
-}
+// export async function GET() {
+//     await mongoose.connect(mongoUrl);
+//     const data = await Product.find({ name: "yash" });
+//     return NextResponse.json({ result: data });
+// }
 
 
 export async function POST(req) {
+    // Extract data from the request's JSON body
     const { name, email, message } = await req.json();
+
+    // Connect to the MongoDB database
     await mongoose.connect(mongoUrl);
-    await Product.create({ name, email, message })
-    return NextResponse.json({ message: "Created" })
+
+    try {
+        // Create a new document in the Product collection
+        await Product.create({ name, email, message });
+
+        return NextResponse.json({ message: "Created" });
+    } finally {
+        // Close the MongoDB connection when the function is done
+        mongoose.disconnect();
+    }
 }
